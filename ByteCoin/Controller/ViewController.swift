1b2fde8374e2e0dccd6f40090b5e88094c210834
stefan <stefanchelaru96@gmail.com>
2020-04-02 18:36:48 +0300

    finished

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        bitcoinLabel.text = coinManager.currencyArray[0]
+        
+        coinManager.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+//MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
+    func didUpdateCurrency(_ coinManager: CoinManager, lastPrice: Double) {
+        DispatchQueue.main.async {
+            self.currencyLabel.text = String(lastPrice)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+        bitcoinLabel.text = selectedCurrency
+    }
+    
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -9,11 +9,51 @@
 import Foundation
 
 struct CoinManager {
-    
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let apiKey = "NTFkMTRhYjM1ZTJkNDlmNzk4OGQ3MTdhNWJmZGIwNDY"
+    var delegate: CoinManagerDelegate?
+    let currencyURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
 
     
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(currencyURL)\(currency)"
+        performRequest(with: urlString)
+    }
+        
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let conf = URLSessionConfiguration.default
+            conf.httpAdditionalHeaders = ["x-ba-key": apiKey]
+            let session = URLSession(configuration: conf)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if(error != nil){
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let coinData = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateCurrency(self, lastPrice: coinData)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            return decodedData.last
+        } catch {
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
 }
